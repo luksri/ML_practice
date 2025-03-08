67e09221b78fd0c7faf27381f5527319ead011de
luksri <63786927+luksri@users.noreply.github.com>
2025-03-08 18:47:24 +0530

    checking in few files

---
+++ ai_boot_camp/Agentic-Ai-Project-main-3/src/langgraphagenticai/ui/streamlitui/display_result.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from langchain_core.messages import HumanMessage,AIMessage,ToolMessage
+import json
+
+
+class DisplayResultStreamlit:
+    def __init__(self,usecase,graph,user_message):
+        self.usecase= usecase
+        self.graph = graph
+        self.user_message = user_message
+
+    def display_result_on_ui(self):
+        usecase= self.usecase
+        graph = self.graph
+        user_message = self.user_message
+        if usecase =="Basic Chatbot":
+            for events in graph.stream({'messages':("user",user_message)}, {"configurable": {"thread_id": "Lakshman"}}, stream_mode="values"):
+                message = events['messages'][-1]
+                if type(message) == HumanMessage:
+                    with st.chat_message("user"):
+                        st.write(message.content)
+                elif type(message)==ToolMessage:
+                    with st.chat_message("ai"):
+                        st.write("Tool Call Start")
+                        st.write(message.content)
+                        st.write("Tool Call End")
+                elif type(message)==AIMessage and message.content:
+                    with st.chat_message("assistant"):
+                        st.write(message.content)
+
+        elif usecase=="Chatbot with Tool":
+             # Prepare state and invoke the graph
+            initial_state = {"messages": [user_message]}
+            res = graph.invoke(initial_state)
+            for message in res['messages']:
+                if type(message) == HumanMessage:
+                    with st.chat_message("user"):
+                        st.write(message.content)
+                elif type(message)==ToolMessage:
+                    with st.chat_message("ai"):
+                        st.write("Tool Call Start")
+                        st.write(message.content)
+                        st.write("Tool Call End")
+                elif type(message)==AIMessage and message.content:
+                    with st.chat_message("assistant"):
+                        st.write(message.content)
+             
